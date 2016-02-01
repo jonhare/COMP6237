edb0ed7042ee60c31b74f6e2d92de9bce6f94e5f
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2016-02-01 23:50:55 +0000

    putting together l6

--- app/src/main/java/uk/ac/soton/ecs/comp6237/l6/Lecture6.java
+++ app/src/main/java/uk/ac/soton/ecs/comp6237/l6/Lecture6.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JSplitPane;
+
 import org.openimaj.content.slideshow.PictureSlide;
 import org.openimaj.content.slideshow.Slide;
 import org.openimaj.content.slideshow.SlideshowApplication;
 
+import uk.ac.soton.ecs.comp6237.utils.GroovyREPLConsoleSlide;
 import uk.ac.soton.ecs.comp6237.utils.Utils;
 import uk.ac.soton.ecs.comp6237.utils.annotations.JvmArgs;
 import uk.ac.soton.ecs.comp6237.utils.annotations.Lecture;
@@ -20,11 +23,24 @@ public class Lecture6 {
 	public static void main(String[] args) throws IOException {
 		final List<Slide> slides = new ArrayList<Slide>();
 
-		for (int i = 1; i <= 55; i++) {
+		for (int i = 1; i <= 56; i++) {
 			slides.add(new PictureSlide(Lecture6.class.getResource(String.format("l6.%03d.jpeg", i))));
 		}
 
-		slides.set(1, new ItemSpaceSlide());
+		slides.set(30, new ItemSpaceSlide());
+		slides.set(33, new UserSpaceSlide());
+
+		slides.set(36,
+				new GroovyREPLConsoleSlide(JSplitPane.HORIZONTAL_SPLIT, Lecture6.class.getResource("../UserSim.groovy"))); // user-user
+		// sim
+		slides.set(39,
+				new GroovyREPLConsoleSlide(JSplitPane.HORIZONTAL_SPLIT, Lecture6.class.getResource("../MovieSim.groovy"))); // user
+		// based rec
+
+		slides.set(42, new UserSpaceSlide()); // item-item sim
+
+		slides.set(45, new UserSpaceSlide()); // item-item precomp
+		slides.set(47, new UserSpaceSlide()); // item-based rec
 
 		new SlideshowApplication(slides, 1024, 768, Utils.BACKGROUND_IMAGE);
 	}
